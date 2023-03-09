8c2eea06ce203964e00ed88facf5892dc804271a
Abu Sakib <mabusakib@gmail.com>
2023-03-09 19:45:01 +0600

    Implement `TryFrom` for `Png`

--- src/png.rs
+++ src/png.rs
@@ -1,4 +1,6 @@
-use crate::chunk::Chunk;
+use std::io::{BufReader, Read};
+
+use crate::{chunk::Chunk, errors::PngError};
 
 /// A PNG file.
 ///
@@ -14,6 +16,7 @@ pub(crate) struct Png {
 
 impl Png {
     const PNG_FILE_SIGNATURE: [u8; 8] = [137, 80, 78, 71, 13, 10, 26, 10];
+    const MIN_CHUNK_LENGTH: usize = 12;
 
     /// Creates a new PNG from some chunks.
     pub(crate) fn from_chunks(chunks: Vec<Chunk>) -> Self {
@@ -29,8 +32,47 @@ impl Png {
     }
 }
 
+impl TryFrom<&[u8]> for Png {
+    type Error = PngError;
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        // First take the signature in a buffer
+        let mut sig_buffer: [u8; 8] = [0; 8];
+        let mut byte_reader = BufReader::new(value);
+        byte_reader.read_exact(&mut sig_buffer)?;
+
+        // The chunks start from the 8th byte of `value` after 
+        // taking into account the signature.
+        let mut starting_cursor = 8;
+        let mut chunks = Vec::<Chunk>::new();
+        let mut end_cursor: usize = 0;
+
+        // While the end cursor doesn't reach the length of the slice.
+        while starting_cursor < value.len() {
+            // Take the `length` field of `Chunk` into a buffer.
+            let mut chunk_length_buffer: [u8; 4] = [0; 4];
+            let mut chunk_length_reader = BufReader::new(&value[starting_cursor..]);
+            chunk_length_reader.read_exact(&mut chunk_length_buffer)?;
+            let chunk_length = u32::from_be_bytes(chunk_length_buffer) as usize;
+
+            // We can now get upto which byte the current chunk exists.
+            end_cursor = Self::MIN_CHUNK_LENGTH + chunk_length + starting_cursor;
+
+            // The chunk exists from `starting_cursor` upto `end_cursor-1`
+            let chunk = Chunk::try_from(&value[starting_cursor..end_cursor])?;
+            chunks.push(chunk);
+            starting_cursor = end_cursor;
+        }
+
+        Ok(Self {
+            signature: sig_buffer,
+            chunks,
+        })
+    }
+}
+
 #[cfg(test)]
 mod pngtests {
+    use std::io::Read;
     use std::str::FromStr;
 
     use super::*;
@@ -71,5 +113,22 @@ mod pngtests {
         let png = Png::from_chunks(chunks);
 
         assert_eq!(png.chunks().len(), 3);
+    }
+
+    #[test]
+    fn test_valid_png_from_bytes() {
+        let chunk_bytes: Vec<u8> = get_testing_chunks()
+            .into_iter()
+            .flat_map(|chunk| chunk.as_bytes())
+            .collect();
+
+        let bytes: Vec<u8> = Png::PNG_FILE_SIGNATURE
+            .iter()
+            .chain(chunk_bytes.iter())
+            .copied()
+            .collect();
+
+        let png = Png::try_from(bytes.as_ref());
+        assert!(png.is_ok());
     }
 }
